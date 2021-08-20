13c9b087fe7590681dcc7a6972e9f1753fde0046
Nikonov Vladzimir <nikonovbox@gmail.com>
2021-08-20 23:48:41 +0300

    mark1

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         int count = 500;
-        int transfer = 200;
+        int transfer = 501;
         int totalCount = count + transfer;
-        int totalBonus = totalCount / 100;
+        int totalBonus;
 
         if (totalCount > 1000) {
-            System.out.println("Баланс: " + totalCount);
-            System.out.println("Итого бонусов: " + totalBonus);
-            System.out.println("Итоговый баланс счета с бонусами: " + (totalCount + totalBonus));
+            totalBonus = totalCount / 100;
+
         } else {
             totalBonus = 0;
-            System.out.println("Баланс: " + totalCount);
-            System.out.println("Итого бонусов: " + totalBonus);
-            System.out.println("Итоговый баланс счета с бонусами: " + totalCount);
-        }
 
+        }
+        System.out.println("Баланс: " + totalCount);
+        System.out.println("Итого бонусов: " + totalBonus);
+        System.out.println("Итоговый баланс счета с бонусами: " + (totalCount + totalBonus));
     }
 }
